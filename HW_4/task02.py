1c70a37ad12ccf9f296890f9a697b43135aee970
IraBarbar <bro998877@yahoo.com>
2023-09-11 21:29:28 +0300

    Dopy

--- HW_4/task02.py
+++ HW_4/task02.py
@@ -1,9 +1,7 @@
 # Напишите функцию принимающую на вход только ключевые
 # параметры и возвращающую словарь, где ключ — значение
 # переданного аргумента, а значение — имя аргумента. Если
-# ключ не хешируем, используйте его строковое представление.
-from pprint import pp
-
+# ключ не хешируем, используйте его строковое представлени
 
 def dict_args(a=20, b='ser', c=(1, 2), d=str([1, 2]), t=str({'Python': 3.1}), h=25.2, l=5) -> dict:
     """Возвращающую словарь, где ключ — значение
@@ -13,13 +11,12 @@ def dict_args(a=20, b='ser', c=(1, 2), d=str([1, 2]), t=str({'Python': 3.1}), h=
     for key, val in locals().items():
         if isinstance(val, list | dict | frozenset | bytearray):
             dict_[(str(val))] = key
-
         else:
             dict_[val] = key
+    dict_.popitem()
     return dict_
 
 
-my_dict = dict_args()
-for i, j in my_dict.items():
-    print(f'{str(i):<95}: {j}')
+for i, j in dict_args().items():
+    print(f'{str(i):<17}: {j}')
 
