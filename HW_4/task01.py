1f1be86140552d972a2c8dbb2375cfebeefde717
IraBarbar <bro998877@yahoo.com>
2023-09-11 21:58:31 +0300

    Dopy

--- HW_4/task01.py
+++ HW_4/task01.py
@@ -3,19 +3,19 @@
 def transposition_matrix(lst: list):
     """ Вывод транспонирования матрицы."""
 
-    lst_j = []
-    lst_i = []
+    lst_temp = []
+    lst_position = []
     for i in range(len(lst[0])):
         for j in range(len(lst)):
-            lst_j.append(lst[j][i])
-        lst_i.append(lst_j)
-        lst_j = []
-    for num in lst_i:
-        print(num)
+            lst_temp.append(lst[j][i])
+        lst_position.append(lst_temp)
+        lst_temp = []
+    return lst_position
 
 
 lst_matrix = [[1, 2, 3, 4, 5], [11, 21, 31, 41, 51]]
 for item in lst_matrix:
     print(item)
 print('*' * 10)
-transposition_matrix(lst_matrix)
+for num in transposition_matrix(lst_matrix):
+    print(num)
